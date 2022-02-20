f2728a8fae91be72357739edbfe8c95ad99122f9
augustspies22 <augustspies22@gmail.com>
2022-02-20 08:39:20 -0800

    settings updated for production 3

--- config/settings.py
+++ config/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
-elif not DEBUG:
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+if not DEBUG:
     STATIC_ROOT = BASE_DIR.joinpath('static')
 
 

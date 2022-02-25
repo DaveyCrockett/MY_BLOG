8beaad21830a6009f3fa435b310dfbfbd7bc4900
augustspies22 <augustspies22@gmail.com>
2022-02-24 17:39:00 -0800

    adjusted settings static for production 2

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
+if DEBUG == True:
     STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')), ]
 STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 
